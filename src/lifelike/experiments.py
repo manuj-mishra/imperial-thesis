325146d1b7d343b87c34cc83dd31fc5b0e769c89
Manuj Mishra <manujmishra2000@gmail.com>
2022-06-07 16:13:25 +0100

    small singleres exp

--- src/lifelike/experiments.py
+++ src/lifelike/experiments.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     "max_step": max_step,
     "eval_step": 10
   }
-  exp_name = f"epoch{epoch_n}_pop{pop_size}"
+  exp_name = f"epoch{epoch_n}_pop{pop_size}_singleres"
   print(f"Running {exp_name}")
   rules = []
   conv_epochs = []
